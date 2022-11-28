21664ffd0300cead00012c8d883230304ca04792
MohamedAmineSafi04 <mohammedameenalsafi@gmail.com>
2022-11-28 08:12:31 +0100

    Linking other django views from your template

--- feed/urls.py
+++ feed/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import HomePageView, PostDetailView
 
 
-app_name = 'feed'
+app_name = 'feed' #MUST MATCH NAMESPACE IN myapp/urls.py
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='index'),
