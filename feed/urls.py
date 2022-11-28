b38b6de1dad5e4480a331be77d353be85b55ef41
MohamedAmineSafi04 <mohammedameenalsafi@gmail.com>
2022-11-28 07:44:35 +0100

    Adding a detail view

--- feed/urls.py
+++ feed/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, PostDetailView
+
 
 app_name = 'feed'
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='index')
+    path('', HomePageView.as_view(), name='index'),
+    path('detail/<int:pk>/', PostDetailView.as_view(), name="detail")
 ]
--- feed/views.py
+++ feed/views.py
@@ -1,7 +1,7 @@
 # from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 from .models import Post
 
 class HomePageView(TemplateView):
@@ -11,3 +11,7 @@ class HomePageView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['posts'] = Post.objects.all()
         return context
+
+class PostDetailView(DetailView):
+    template_name = "detail.html"
+    model = Post
